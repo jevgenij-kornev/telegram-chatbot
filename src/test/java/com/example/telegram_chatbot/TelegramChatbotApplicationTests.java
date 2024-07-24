c40df1f3aed26a409d1ca7596867d557718275f6
jevgenijkornev <jevgenij.kornev@gmail.com>
2024-07-24 14:31:25 +0200

    TGM - initial commit.

---
+++ src/test/java/com/example/telegram_chatbot/TelegramChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
